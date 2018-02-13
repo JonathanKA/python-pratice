9bad28f0e72427b22dced8d403cf7d87c66219d3
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-13 18:49:36 -0200

    Added the exercise question

--- ex016.py
+++ ex016.py
@@ -6,6 +6,7 @@
 #print('A Raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
 #print(emoji.emojize("olá, mundo :earth_americas:", use_aliases=True))
 
+#Crie um programa que leia um numero real qualquer pelo teclado e mostra na tela a sua porção inteira.
 from math import trunc
 n1 = float(input('Digit um valor com casas decimáis: '))
 inteiro = trunc(n1)
