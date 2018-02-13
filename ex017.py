a330913d0ddeae663137a4b84b5fabfd62a70f55
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-13 19:16:01 -0200

    Added the question statement Added the regular code to solve this question. Formatted the text in the print.

--- ex017.py
+++ ex017.py
@@ -1,3 +1,8 @@
+#ex017
+#Faça um programa que leia o comprimento do cateto oposto e do cateto adjancente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
+
+# hipotenusa = (n1 ** 2 + n2 ** 2) ** (1/2) #forma normal de fazer.
+
 import math
 n1 = float(input('Digite o valor do cateto oposto do triângulo retângulo: '))
 n2 = float(input('Digite o valor do cateto adjacente: '))
@@ -6,5 +11,5 @@ hipotenusa = math.hypot(n1, n2)
 hipo2 = math.sqrt(math.pow(n1, 2) + math.pow(n2, 2))
 #quadrado da hipotenusa = soma dos quadrados dos catetos a²+b²=c²
 
-print('O valor da hipotenusa é {}'.format(hipotenusa))
+print('O valor da hipotenusa é {:.2f}'.format(hipotenusa))
 print(hipo2)
