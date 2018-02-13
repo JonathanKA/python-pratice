57c3b8c6eda9640a1ad4afb562103582fbab22be
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-13 12:00:02 -0200

    Updated print text. Added a new formula for reminder.

--- ex012.py
+++ ex012.py
@@ -1,3 +1,3 @@
 preco = float(input('Digite o preco do produto:\n'))
-desconto = preco - (preco * 0.05)
+desconto = preco - (preco * 0.05) # ou desconto = preco -( preco * 5 /100)
 print('O preço com 5% de desconto é R${:.2f}'.format(desconto))
