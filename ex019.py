6bb8d5b71a71acf97e2d38853b171e6aa9ba14ad
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-13 18:29:41 -0200

    Initial ex019 code commit.

--- ex019.py
+++ ex019.py
@@ -1,15 +1,23 @@
 import random
 n1 = random.randint(0,3)
+#Ou pode resolver assim
+n2 = input('Digite o primeiro nome: ')
+n3 = input('Digite o segundo nome: ')
+n4 = input('Digite o terceiro nome: ')
+n5 = input('Digite o quarto nome: ')
 
-alunos = ['Joao', 'Claudio', 'Pedro', 'Miguel']
+#alunos = ['Joao', 'Claudio', 'Pedro', 'Miguel']
 
 if n1 == 0:
-    print('Joao')
+    print(n2)
 if n1 == 1:
-    print('Claudio')
+    print(n3)
 if n1 == 2:
-    print('Pedro')
+    print(n4)
 if n1 == 3:
-    print('Miguel')
+    print(n5)
 
-print(alunos[n1])
+#print(alunos[n1])
+print(n1)
+
+
