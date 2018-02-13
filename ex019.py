eaa604d2f1c289512756560933b84d5f6bd43724
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-13 17:57:09 -0200

    Initial ex019 code commit. Solved using 2 ways

---
+++ ex019.py
@@ -0,0 +1,15 @@
+import random
+n1 = random.randint(0,3)
+
+alunos = ['Joao', 'Claudio', 'Pedro', 'Miguel']
+
+if n1 == 0:
+    print('Joao')
+if n1 == 1:
+    print('Claudio')
+if n1 == 2:
+    print('Pedro')
+if n1 == 3:
+    print('Miguel')
+
+print(alunos[n1])
