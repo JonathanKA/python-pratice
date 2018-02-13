96e8d439fb191473cfef38129bcdc4c124b13998
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-13 19:24:45 -0200

    Added the question statement Added another print to show the result in radians.

--- ex018.py
+++ ex018.py
@@ -1,9 +1,13 @@
+#ex018
+#Faça um programa que leia umângulo qualquer e mostra na tela o valor do seno, cosseno e tangente desse ângulo
+
 from math import sin, cos, tan, radians, degrees
 n1 = radians(float(input('Digite um angulo qualquer: ')))
 #Calculating the sin, cos and tan
 
-seno = degrees(sin(n1))
-cose = degrees(cos(n1))
-tang = degrees(tan(n1))
+seno = sin(n1)
+cose = cos(n1)
+tang = tan(n1)
 
-print('O valor do seno é: {:.2f}\nO valor do cosceno é: {:.2f}\nO valor da tangente é: {:.2f}'.format(seno, cose, tang))
+print('O valor do seno em graus é: {:.2f}\nO valor do cosceno em graus é: {:.2f}\nO valor da tangente em graus é: {:.2f}'.format(degrees(seno), degrees(cose), degrees(tang)) )
+print('O valor em radianos do seno é {:.2f}, do cosseno é {:.2f} e da tagente é {:.2f}'.format(seno, cose, tang) )
