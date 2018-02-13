5911c89f54f08aa9b9ea076a4e0bf764ccaf2ebf
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-13 13:02:00 -0200

    Added another code to calculate the conversion.

--- ex014.py
+++ ex014.py
@@ -1,3 +1,4 @@
 cel = float(input('Type the value in Celsius:\n'))
-faren = (cel * 1.8) + 32
+faren = (cel * 1.8) + 32 #sintaxe da internet que tbm funciona
+#faren = ((9*cel)/5)+32 #formula correta
 print('The temperature in Celsius was {:.2f}°c in Farenheit is {:.2f}°f'.format(cel, faren))
