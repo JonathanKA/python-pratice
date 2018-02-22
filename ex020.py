17fef7a613255096b1b77aba20751d0304e32bea
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-21 21:00:15 -0300

    Added the question statement Added the teacher solution

--- ex020.py
+++ ex020.py
@@ -1,4 +1,7 @@
 #ex020
+#Sorteando uma ordem na lista
+#O mesmo Prof. do desafio anterior quer sortear a ordem de aprensentação de trabalhos dos alunos. Faça um progrma que leia o nome dos quatro alunos e mostre a ordem sorteada
+
 import random
 
 n2 = input('Digite o primeiro nome: ')
@@ -7,5 +10,11 @@ n4 = input('Digite o terceiro nome: ')
 n5 = input('Digite o quarto nome: ')
 
 n1 = random.sample([n2, n3, n4, n5], k=4)
+#resolução do prof.
+lista = [n5, n2, n3, n4]
+random.shuffle(lista)
+
 
 print(n1)
+print(lista)
+
