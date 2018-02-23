7ae1c07229ba63bf48eedf3e15eca9cb842253f6
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-23 18:21:56 -0300

    Solved the exercise.

--- ex022.py
+++ ex022.py
@@ -2,13 +2,22 @@
 #Crie um programa que leia o nome completo de uma pessoa e mostre:
 #-O nome com todas as Letras maiusculas
 #-O nome com todas as minusculas
-#-Quantas letras ao todos(sem considerar espacoes)
+#-Quantas letras ao todo(sem considerar espacos)
 #-Quantas letras tem o primeiro nome
 
-frase = 'Curso em Video Python'
-print('Curso' in frase)
-print(frase.find('Video'))
+name = str(input('Digite um nome completo: '))
 
-dividido = frase.split()
-print(dividido)
-print(dividido[2][3])
+print(name.upper())
+print(name.lower())
+print(len(name.replace(' ','')))
+name1 = name.split()
+print(len(name1[0]))
+
+
+# frase = 'Curso em Video Python'
+# print('Curso' in frase)
+# print(frase.find('Video'))
+#
+# dividido = frase.split()
+# print(dividido)
+# print(dividido[2][3])
