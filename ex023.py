0cab5888f233ea0b325ff1d917ef14fd290b85b5
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-23 18:37:17 -0300

    Solved the exercise.

--- ex023.py
+++ ex023.py
@@ -1,8 +1,15 @@
 #ex023
 
-#Faca um progrma que leioa um numero de 0 a 9999 e mostre na tela cada um dos digitos separadaos.
+#Faca um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos separadaos.
 #ex: Digite um numero: 1834
 #unidade: 4
 #dezena: 3
 #centena: 8
 #milhar: 1
+
+numero = input('Digite um numero inteiro de 0 a 9999: ')
+
+print('Milhar: {}'.format(numero[0]))
+print('Centena: {}'.format(numero[1]))
+print('Dezena: {}'.format(numero[2]))
+print('Unidade: {}'.format(numero[3]))
