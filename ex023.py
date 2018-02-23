fb88e5e54290ccf00ac46d67ad64b06bb7ab86d7
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-23 18:02:21 -0300

    Initial commit

---
+++ ex023.py
@@ -0,0 +1,8 @@
+#ex023
+
+#Faca um progrma que leioa um numero de 0 a 9999 e mostre na tela cada um dos digitos separadaos.
+#ex: Digite um numero: 1834
+#unidade: 4
+#dezena: 3
+#centena: 8
+#milhar: 1
