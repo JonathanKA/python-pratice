7610e494572735123e77cbb28335d4fc88b25932
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-24 11:58:02 -0300

    Solved the exercise

--- ex026.py
+++ ex026.py
@@ -3,3 +3,14 @@
 #Quantas vezes aparece a letra "A"
 #Em que posicao ela aparece a primeira vez.
 #Em que posicao ela aparece a ultima vez.
+
+frase = str(input('Digite uma frase qualquer: '))
+
+check1 = frase.count('a')
+print('Quantas vezes aparece a letra "a": {}'.format(check1))
+
+check2 = frase.find('a')
+print('Ela aparece a primeira vez na casa: {}'.format(check2))
+
+check3 = frase.rfind('a')
+print('Ela aparece a ultima vez na casa: {}'.format(check3))
