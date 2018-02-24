7bf258441baad61aba23870823bb6e7e4edcb1d1
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-24 16:32:31 -0300

    Added uppercase and lowercase check

--- ex025.py
+++ ex025.py
@@ -2,6 +2,9 @@
 #Crie um programa que leia o nome d euma pessoa e diga se ela tem "SILVA no nome.
 
 nome = str(input('Digite o nome completo de uma pessoa: '))
+
+#Esse nao trata se existe diferenca de upper ou lowercase
 print('Existe o nome silva no caractere {}'.format(nome.find('Silva')))
 # checagem = 'Silva' in nome
-print('Existe Silva no nome? ', 'Silva' in nome)
+n1 = nome.upper()
+print('Existe Silva no nome? ', 'SILVA' in n1)
