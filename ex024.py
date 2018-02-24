795bb0536148db6fb234fbeb435bf593e8a68ebf
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-24 14:20:59 -0300

    improved the check added another resolution

--- ex024.py
+++ ex024.py
@@ -1,6 +1,10 @@
 #ex024
 #Crie um programa que leia o nome de uma cidade  e diga se  ela comeca ou nao com nome "SANTO"
-cidade = str(input('Digite o nome de uma Cidade :'))
-lista = cidade.split()
+cidade = str(input('Digite o nome de uma Cidade :')).strip()
+lista = cidade.upper().split()
 
-print('Santo' in lista[0])
+print('SANTO' in lista[0])
+
+#Resolucao do exercicios
+
+print(cidade[:5].upper() == 'SANTO')
