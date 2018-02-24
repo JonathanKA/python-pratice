1cc75973c5ad9646101110f4e137d3aee1203bd4
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-24 16:44:03 -0300

    improved the code for errors changed from index position to normal position for readability

--- ex026.py
+++ ex026.py
@@ -4,13 +4,13 @@
 #Em que posicao ela aparece a primeira vez.
 #Em que posicao ela aparece a ultima vez.
 
-frase = str(input('Digite uma frase qualquer: '))
+frase = str(input('Digite uma frase qualquer: ').lower().strip())
 
 check1 = frase.count('a')
 print('Quantas vezes aparece a letra "a": {}'.format(check1))
 
-check2 = frase.find('a')
+check2 = frase.find('a') + 1
 print('Ela aparece a primeira vez na casa: {}'.format(check2))
 
-check3 = frase.rfind('a')
+check3 = frase.rfind('a') + 1
 print('Ela aparece a ultima vez na casa: {}'.format(check3))
