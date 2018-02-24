1d80752e44fb4313a48a06cbcfda6c908e7adfab
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-24 11:36:10 -0300

    Added 2 resolutions, can be improved.

--- ex025.py
+++ ex025.py
@@ -1,3 +1,7 @@
 #ex025
 #Crie um programa que leia o nome d euma pessoa e diga se ela tem "SILVA no nome.
-# "
+
+nome = str(input('Digite o nome completo de uma pessoa: '))
+print('Existe o nome silva no caractere {}'.format(nome.find('Silva')))
+# checagem = 'Silva' in nome
+print('Existe Silva no nome? ', 'Silva' in nome)
