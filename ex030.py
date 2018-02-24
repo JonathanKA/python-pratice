25664f4b34db0cbf3a749c0f3c3e9f75d679da2f
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-24 18:30:12 -0300

    initial commit

---
+++ ex030.py
@@ -0,0 +1,8 @@
+#ex030
+#Criei um programa que leia um numero inteiro e mostre na tela se ele é PAR ou ÍMPAR
+
+num = int(input('Digite um numero inteiro: '))
+if num % 2 == 0:
+    print('O número {} é par.'.format(num))
+else:
+    print('O número {} é impár.'.format(num))
