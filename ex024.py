dc47214e1bd757344c2c247e190c2e7749fb7ab7
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-24 11:25:28 -0300

    Solved the question, basic resolution can be greatly improved.

--- ex024.py
+++ ex024.py
@@ -1,2 +1,6 @@
 #ex024
 #Crie um programa que leia o nome de uma cidade  e diga se  ela comeca ou nao com nome "SANTO"
+cidade = str(input('Digite o nome de uma Cidade :'))
+lista = cidade.split()
+
+print('Santo' in lista[0])
