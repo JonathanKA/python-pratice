78cac16e46d2762526d40231791a4cedc231152a
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-25 12:19:18 -0300

    added an option to pull the current year from the system.

--- ex032.py
+++ ex032.py
@@ -1,7 +1,9 @@
 #ex032
 #Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO
-ano = int(input('Digite um ano qualquer:\n'))
-
+import datetime
+ano = int(input('Digite um ano qualquer ou digite 0 caso queira analisar o ano atual:\n'))
+if ano == 0:
+    ano = datetime.date.today().year
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('Esse ano é Bissexto!')
 else:
