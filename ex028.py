1354efc8fc71b06f6dbfea49831b1c432d002074
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-25 12:00:15 -0300

    made some text changes. added time module

--- ex028.py
+++ ex028.py
@@ -4,14 +4,17 @@
 
 #my code
 import random
+import time
 
-n1 = random.randint(0,5)
-
+n1 = random.randint(0, 5)
+print('-=-' * 20)
 print('Desafio!!\nTenter advinhar qual numero o computador escolheu!')
+print('-=-' * 20)
 
-user = int(input('Digite aqui o valor que você acha que o computador escolheu: '))
-
+user = int(input('Em que número eu pensei?\n'))
+print('Processando...')
+time.sleep(2)
 if user == n1:
-    print('Parabéns! Você acertou!!')
+    print('Parabéns! Você acertou!! Nós pensamos no número {}'.format(user))
 else:
-    print('Que pena você errou!')
+    print('Que pena você errou! Eu pensei no número {} e não no {}.'.format(n1, user))
