99ca523fc4a8f8b852b3ffe8411fd5cb2851bed7
Jonathan Knopp <jonny.knopp@gmail.com>
2018-02-26 12:20:58 -0300

    Added Solution 2 Added Solution 3

--- ex033.py
+++ ex033.py
@@ -5,21 +5,33 @@ print('Digite 3 números diferentes!')
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 n3 = int(input('Digite o terceiro número: '))
-if n1 > n2 and n1 > n3:
-    if n2 > n3:
-        print('O maior número é {} e o menor número é {}'.format(n1, n2))
-    else:
-        print('O maior número é {} e o menor número é {}'.format(n1, n3))
-
+#verificando o menor
+menor = n1
+if n2 < n1 and n3 < n1:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+#verificando o maior
+maior = n1
 if n2 > n1 and n2 > n3:
-    if n1 > n3:
-        print('O maior número é {} e o menor número é {}'.format(n2, n1))
-    else:
-        print('O maior número é {} e o menor número é {}'.format(n2, n3))
-
+    maior = n2
 if n3 > n1 and n3 > n2:
-    if n1 > n2:
-        print('O maior número é {} e o menor número é {}'.format(n3, n1))
-    else:
-        print('O maior número é {} e o menor número é {}'.format(n3, n2))
-print('--END IFs---')
+    maior = n3
+print('Menor: {}'.format(menor))
+print('Maior: {}'.format(maior))
+
+#Solution 2
+print(min(n1, n2, n3))
+print(max(n1, n2, n3))
+
+#Solution 3
+print('Digite 3 números diferentes!')
+
+# n1 = int(input('Digite o primeiro número: '))
+# n2 = int(input('Digite o segundo número: '))
+# n3 = int(input('Digite o terceiro número: '))
+
+list1 = [n1,n2,n3]
+
+list1.sort(reverse=True)
+print('o menor é:',min(list1),'o maior é:', max(list1))
