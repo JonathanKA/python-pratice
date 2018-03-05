7889922a2f462f4cecaff838cb7884c50271f096
Jonathan Knopp <jonny.knopp@gmail.com>
2018-03-04 21:01:21 -0300

    improved the code.

--- ex033.py
+++ ex033.py
@@ -5,21 +5,22 @@ print('Digite 3 números diferentes!')
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 n3 = int(input('Digite o terceiro número: '))
-if n1 > n2 and n1 > n3:
-    if n2 > n3:
-        print('O maior número é {} e o menor número é {}'.format(n1, n2))
-    else:
-        print('O maior número é {} e o menor número é {}'.format(n1, n3))
 
-if n2 > n1 and n2 > n3:
-    if n1 > n3:
-        print('O maior número é {} e o menor número é {}'.format(n2, n1))
-    else:
-        print('O maior número é {} e o menor número é {}'.format(n2, n3))
-
-if n3 > n1 and n3 > n2:
-    if n1 > n2:
-        print('O maior número é {} e o menor número é {}'.format(n3, n1))
-    else:
-        print('O maior número é {} e o menor número é {}'.format(n3, n2))
-print('--END IFs---')
+#Verificando o menor
+a = n1
+b = n2
+c = n3
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+#Verificando o maior
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+print('===' * 10)
+print('maior é ', maior)
+print('menor é ', menor)
