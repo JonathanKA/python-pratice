a94f0205182d9d96a1c8d00d0e93bbc37c092c2e
Jonathan Knopp <jonny.knopp@gmail.com>
2018-03-11 00:24:32 -0300

    Class 11 Bonus how to color the terminal

---
+++ ex_mundo_1_bonus.py
@@ -0,0 +1,21 @@
+###
+#Style
+#0 none
+#1 bold
+#4 underline
+#7 negative(inverse)
+
+#text
+#30 white
+#31 red
+#32 green
+#33 yellow
+#34 blue
+#35 purple
+#36 azul claro
+#37 cinza
+
+print('\033[4;37;44mOlá, Mundo!\033[m')
+a = 1
+b = 4
+print('os Valores são \033[32m{}\033[m e \033[31m{}\033[m.'.format(a , b ))
